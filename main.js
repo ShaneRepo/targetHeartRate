01b45b12399ed2f3791bb8ce8e7e7369b071db4d
Shane <shane8641@live.com>
2016-02-13 14:10:13 -0500

    added parseInt to result string

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 function calcHeartRate(){
     var user = document.getElementById("age").value;
     var maxRate = 220 - user;
-    var minTarget = maxRate * .5;
-    var maxTarget = maxRate * .85;
+    var minT = maxRate * .5;
+    var maxT = maxRate * .85;
+    var minTarget = parseInt(minT);
+    var maxTarget = parseInt(maxT);
     document.getElementById("result").value = minTarget + " to " + maxTarget + " beats per minute.";
 }
 document.getElementById("submit").addEventListener("click", calcHeartRate, false);
